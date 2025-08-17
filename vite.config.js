b7db8c5d977c911c9146e9dbfa6adac6c5d08187
QLiMBer <qlimber@users.noreply.github.com>
2025-08-18 00:54:49 +0200

    chore(config): remove stale JS Vite config and d.ts; use TS config only

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,7 @@ export default defineConfig({
         globals: true,
         setupFiles: 'src/test/setup.ts',
         coverage: {
+            provider: 'v8',
             reporter: ['text', 'html', 'lcov'],
         },
     },
